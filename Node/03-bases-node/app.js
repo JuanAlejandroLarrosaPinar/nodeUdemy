285d75ac033d32d7bf8a63c1cfa2706d1ac61a33
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-17 15:43:00 +0200

    32. Recibir información desde línea de comando

--- Node/03-bases-node/app.js
+++ Node/03-bases-node/app.js
@@ -2,7 +2,11 @@ const {crearArchivo} = require('./helpers/multiplicar')
 
 console.clear();
 
-const base = 9;
+console.log(process.argv);
+const [,,arg3='base=5'] = process.argv;
+const [, base=5] = arg3.split('=');
+console.log(base);
+
 crearArchivo(base).then(nombreArchivo=>console.log(nombreArchivo, 'creado')).catch('Error al crear fichero');
 
 
