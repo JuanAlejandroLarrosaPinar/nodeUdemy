f207ff07a92cdf786c06be4076c8cdc2124bb2a7
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-17 15:06:51 +0200

    30. Requerir paquetes - require

--- Node/03-bases-node/app.js
+++ Node/03-bases-node/app.js
@@ -1,11 +1,22 @@
+const fs = require('fs');
+
 console.clear();
 
-const base = 6;
+const base = 3;
 
 console.log('----------------------------')
 console.log(`Tabla del ${base}`);
 console.log('----------------------------')
 
+let salida = '';
 for(let i=1;i<=10;i++){
-    console.log(`${base} x ${i} = ${base*i}`);
+    //console.log(`${base} x ${i} = ${base*i}`);
+    salida+= `${base} x ${i} = ${base*i}\n`;
 }
+
+//console.log('Salida: ',salida);
+const nombreFichero = `tabla-${base}.txt`;
+fs.writeFile(nombreFichero, salida, (err)=>{
+    if(err) throw err;
+    console.log(`${nombreFichero} creado`);
+});
