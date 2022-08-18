e3dd8141d84dd6d8cc9c4184325e21dcc9e33925
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-18 12:34:12 +0200

    51. Lógica para el manejo de tareas por hacer

--- Node/04-tareas-hacer/app.js
+++ Node/04-tareas-hacer/app.js
@@ -1,18 +1,23 @@
 const { inquirerMenu } = require('./helpers/inquirer');
+require('colors');
 const { pausa } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 //const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
-
-require('colors');
-
-console.clear();
 const main = async () =>{
     console.log('Hola mundo');
 
     let opt = '';
     do{
-        opt = await inquirerMenu();
+        //opt = await inquirerMenu();
         //console.log(opt);
+        const tareas = new Tareas();
+        const tarea = new Tarea('Comprar comida');
+
+        tareas._listado[tarea.id] = tarea;
+        console.log(tarea);
+        console.log(tareas);
         if(opt!=='0') await pausa();
     }while(opt!=='0')
     
