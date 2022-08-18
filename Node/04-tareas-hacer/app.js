c24d46a72865a7f7c8bfe08095f2277a7d4445bc
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-18 11:50:51 +0200

    47. Repetir el menú de forma infinita

--- Node/04-tareas-hacer/app.js
+++ Node/04-tareas-hacer/app.js
@@ -5,8 +5,15 @@ require('colors');
 console.clear();
 const main = async () =>{
     console.log('Hola mundo');
-    mostrarMenu();
-    pausa();
+
+    let opt = '';
+    do{
+        opt = await mostrarMenu();
+        console.log(opt);
+        if(opt!=='0') await pausa();
+    }while(opt!=='0')
+    
+    //pausa();
 }
 
 main();
