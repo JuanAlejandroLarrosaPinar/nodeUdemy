f663b7a528ff127deddff012b0608c468c10acce
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-19 13:35:49 +0200

    87. Request y response

--- Node/06-webserver/app.js
+++ Node/06-webserver/app.js
@@ -1,7 +1,29 @@
 const http = require('http');
 
-http.createServer((request, response)=>{
-    response.write('Hola mundo');
-    response.end();
+http.createServer((req, res)=>{
+    console.clear();
+    console.log(req);
+    //texto plano
+    /*res.writeHead(200, {'Content-Type':'text/plain'}); //201, 404 //text/plain
+    const persona = {
+        id:1,
+        nombre: 'Juan Alejandro'
+    }
+    res.write(JSON.stringify(persona));*/
+    //json
+    /*res.writeHead(200, {'Content-Type':'application/json'}); //201, 404 //text/plain
+    const persona = {
+        id:1,
+        nombre: 'Juan Alejandro'
+    }
+    res.write(JSON.stringify(persona));*/
+    //csv
+    res.setHeader('Content-Disposition','attachment; filename=lista.csv');
+    res.writeHead(200,{'Content-Type': 'application/csv'});
+    res.write('id, nombre\n');
+    res.write('1, Juan Alejandro\n');
+
+
+    res.end();
 })
 .listen(8080)
