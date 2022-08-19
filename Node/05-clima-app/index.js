b848acd1e8c7f282f7356fdc8e332bf62fa27bac
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-19 10:28:09 +0200

    69. Modelo para controlar la aplicación

--- Node/05-clima-app/index.js
+++ Node/05-clima-app/index.js
@@ -1,11 +1,29 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async()=>{
+    const busquedas = new Busquedas();
     let opt = '';
     do{
         opt = await inquirerMenu();
         switch(opt){
             case '1':
+                //Mostrar mensaje
+                const lugar = await leerInput('Ciudad: ');
+                //Buscar los lugares
+
+                //Seleccionar el lugar
+
+                //Datos del clima
+
+                //Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad: ');
+                console.log('Lat: ');
+                console.log('Long: ');
+                console.log('Temperaturas: ');
+                console.log('Min: ');
+                console.log('Max: ');
                 break;
             case '2':
                 break;
---
+++ Node/05-clima-app/models/busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas{
+    historial = [];
+
+    constructor(){
+        // TODO: 
+    }
+
+    async ciudad(lugar = ''){
+        // peticion http
+        console.log(lugar);
+
+        return []; //retornar los lugares
+    }
+}
+
+module.exports = Busquedas;
