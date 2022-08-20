a91c7f957b17060a5c1639387c913a124457ae51
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-20 16:21:09 +0200

    107. Obtener datos de un POST

--- Node/07-restserver/controllers/usuarios.js
+++ Node/07-restserver/controllers/usuarios.js
@@ -1,5 +1,5 @@
-const {response} = require('express');
-const usuariosGet = (req, res=response) => {
+const {response, request} = require('express');
+const usuariosGet = (req = request, res=response) => {
     //res.send('Hello world')
     res.status(200).send({
         ok: true,
@@ -16,10 +16,14 @@ const usuariosPut = (req, res) => {
 }
 
 const usuariosPost = (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const {nombre, edad} = body;
     //res.send('Hello world')
     res.status(200).send({
         ok: true,
-        msg: "post API - controlador"
+        msg: "post API - controlador",
+        nombre, edad
     });
 }
 
--- Node/07-restserver/models/server.js
+++ Node/07-restserver/models/server.js
@@ -15,6 +15,9 @@ class Server {
     middleWares() {
         //CORS
         this.app.use(cors());
+
+        //Lectura y parseo del body
+        this.app.use(express.json());
         //Directorio publico
         this.app.use(express.static('public'));
     }
