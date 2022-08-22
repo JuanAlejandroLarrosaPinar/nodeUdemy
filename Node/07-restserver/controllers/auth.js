d70f3fcbd2e123d0e39d65f0a2508b753dc9a035
jalarrosap <jalarrosap@eservicios.indra.es>
2022-08-22 17:48:20 +0200

    141. Login de usuario

--- Node/07-restserver/controllers/auth.js
+++ Node/07-restserver/controllers/auth.js
@@ -1,9 +1,46 @@
 const {request, response} = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const login = (req = request, res = response) =>{
-    res.json({
-        msg:'ok'
-    });
+const login = async (req = request, res = response) =>{
+    const {correo, password} = req.body;
+
+    //Verificar si el email existe
+    const usuario = await Usuario.findOne({correo});
+    if(!usuario){
+        return res.status(400).json({
+            msg: 'Usuario / Password no son correctos - correo'
+        });
+    }
+    //Si el usuario está activo
+    if(!usuario.estado){
+        return res.status(400).json({
+            msg: ' Usuario / Password no son correctos - estado: false'
+        })
+    }
+    //Verificar la contraseña
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if(!validPassword){
+        return res.status(400).json({
+            msg: 'Usuario / Password no son correctos - password'
+        })
+    }
+    //Generar el JWT
+    
+    try{
+        res.json({
+            msg:'ok',
+            correo,
+            password
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
+    
+    
 }
 
 module.exports = {
