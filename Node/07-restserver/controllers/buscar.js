6132846033924e82d473812527d1f55a42677403
jalarrosap <jalarrosap@eservicios.indra.es>
2022-09-01 18:40:45 +0200

    175. Búsquedas en base de datos

--- Node/07-restserver/controllers/buscar.js
+++ Node/07-restserver/controllers/buscar.js
@@ -1,10 +1,47 @@
-const { request } = require("express");
+const { request, response } = require("express");
+const { Usuario } = require("../models");
+const {ObjectId} = require('mongoose').Types;
+
+const coleccionesPermitidas = [
+    'categoria',
+    'productos',
+    'roles',
+    'usuarios'
+    
+];
+
+const buscarUsuarios = async(termino= '', res= response)=>{
+    const esMongoId = ObjectId.isValid(termino);
+    if(esMongoId){
+        const usuario = await Usuario.findById(termino);
+        res.json({
+            results: (usuario)?[usuario]: []
+        })
+    }
+}
 
 const buscar = (req= request, res = response)=>{
     const {coleccion, termino} = req.params;
-    res.json({
-        coleccion, termino
-    });
+
+    if(!coleccionesPermitidas.includes(coleccion)){
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`
+        });
+    }
+
+    switch(coleccion){
+        case 'categoria':
+            break;
+        case 'productos':
+            break;
+        case 'usuarios':
+            buscarUsuarios(termino, res);
+            break;
+        default:
+            res.status(500).json({
+                msg:'Se le olvido hacer esta búsqueda'
+            })
+    }
 }
 
 module.exports = {
