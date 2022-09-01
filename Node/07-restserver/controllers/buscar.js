46b3cfaa5eeeb3f6318f0ab4c7b85f3f3cce7a99
jalarrosap <jalarrosap@eservicios.indra.es>
2022-09-01 20:59:55 +0200

    177. Buscar en otras colecciones

--- Node/07-restserver/controllers/buscar.js
+++ Node/07-restserver/controllers/buscar.js
@@ -1,37 +1,37 @@
 const { request, response } = require("express");
-const { Usuario } = require("../models");
-const {ObjectId} = require('mongoose').Types;
+const { Usuario, Categoria, Producto } = require("../models");
+const { ObjectId } = require('mongoose').Types;
 
 const coleccionesPermitidas = [
     'categoria',
     'productos',
     'roles',
     'usuarios'
-    
+
 ];
 
-const buscarUsuarios = async(termino= '', res= response)=>{
+const buscarUsuarios = async (termino = '', res = response) => {
     const esMongoId = ObjectId.isValid(termino);
-    if(esMongoId){
+    if (esMongoId) {
         const usuario = await Usuario.findById(termino);
         return res.json({
-            results: (usuario)?[usuario]: []
+            results: (usuario) ? [usuario] : []
         })
     }
 
     const regex = new RegExp(termino, 'i'); //para el case sensitive
     const usuarios = await Usuario.find({
         $or: [ //para meter la condicion or, igual que en sql
-            {nombre: regex}, //mandamos la expresion regular en lugar del término
-            {correo: regex}
+            { nombre: regex }, //mandamos la expresion regular en lugar del término
+            { correo: regex }
         ],
-        $and:[
-            {estado:true}
+        $and: [
+            { estado: true }
         ]
-        
 
-        
-        
+
+
+
     });
 
     return res.json({
@@ -39,26 +39,67 @@ const buscarUsuarios = async(termino= '', res= response)=>{
     })
 }
 
-const buscar = (req= request, res = response)=>{
-    const {coleccion, termino} = req.params;
+const buscarCategorias = async (termino = '', res = response) => {
+    const esMongoId = ObjectId.isValid(termino);
+    if (esMongoId) {
+        const categoria = await Categoria.findById(termino);
+        return res.json({
+            results: (categoria) ? [categoria] : []
+        })
+    }
 
-    if(!coleccionesPermitidas.includes(coleccion)){
+    const regex = new RegExp(termino, 'i'); //para el case sensitive
+    const categorias = await Categoria.find({
+        nombre: regex, estado: true
+    });
+
+    return res.json({
+        results: categorias
+    })
+}
+
+const buscarProductos = async (termino = '', res = response) => {
+    const esMongoId = ObjectId.isValid(termino);
+    if (esMongoId) {
+        const producto = await Producto.findById(termino).populate('categoria', 'nombre');
+        return res.json({
+            results: (producto) ? [producto] : []
+        })
+    }
+
+    const regex = new RegExp(termino, 'i'); //para el case sensitive
+    const productos = await Producto.find({
+        nombre: regex, estado: true
+        //, ObjectId : '6310d782039853b3c152c9d9'// por si queremos filtrar por la categoría
+    }).populate('categoria', 'nombre');;
+
+    return res.json({
+        results: productos
+    })
+}
+
+const buscar = (req = request, res = response) => {
+    const { coleccion, termino } = req.params;
+
+    if (!coleccionesPermitidas.includes(coleccion)) {
         return res.status(400).json({
             msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`
         });
     }
 
-    switch(coleccion){
+    switch (coleccion) {
         case 'categoria':
+            buscarCategorias(termino, res);
             break;
         case 'productos':
+            buscarProductos(termino, res);
             break;
         case 'usuarios':
             buscarUsuarios(termino, res);
             break;
         default:
             res.status(500).json({
-                msg:'Se le olvido hacer esta búsqueda'
+                msg: 'Se le olvido hacer esta búsqueda'
             })
     }
 }
