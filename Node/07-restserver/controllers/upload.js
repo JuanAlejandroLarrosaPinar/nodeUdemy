156f6895c28c0bbed2e386a39e1c045fc2ea0935
jalarrosap <jalarrosap@eservicios.indra.es>
2022-09-02 16:55:33 +0200

    189. Actualizar imagen de usuario

--- Node/07-restserver/controllers/upload.js
+++ Node/07-restserver/controllers/upload.js
@@ -1,5 +1,7 @@
 const { response, request } = require("express");
+const { model } = require("mongoose");
 const { subirArchivo } = require("../helpers/subir-archivo");
+const { Usuario, Producto } = require("../models");
 const cargarArchivo = async (req = request, res = response) => {
     
     
@@ -25,8 +27,41 @@ const cargarArchivo = async (req = request, res = response) => {
 
 const actualizarImagen = async (req= request, res = response)=>{
     const {id, coleccion} = req.params;
+
+    let modelo;
+
+    switch(coleccion){
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if(!modelo){
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+            
+
+
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if(!modelo){
+                return res.status(400).json({
+                    msg: `No existe un producto con le id ${id}`
+                })
+            }
+            break;
+        default:
+            return res.status(500).json({
+                msg: 'Se me olvidó validar esto'
+            });
+    }
+
+    modelo.img = await subirArchivo(req.files, undefined, coleccion);
+            
+    await modelo.save();
+
     res.json({
-        id, coleccion
+        modelo
     })
 }
 
