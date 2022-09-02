e149aff9e2a41d6e2cab1d75d70a76670d49afde
jalarrosap <jalarrosap@eservicios.indra.es>
2022-09-02 15:40:57 +0200

    184. Validar la extensión

--- Node/07-restserver/controllers/upload.js
+++ Node/07-restserver/controllers/upload.js
@@ -11,7 +11,24 @@ const cargarArchivo = (req = request, res = response) => {
     }
 
     const {archivo} = req.files;
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+    const nombreCortado = archivo.name.split('.');
+    console.log(nombreCortado);
+    const extension = nombreCortado[nombreCortado.length-1];
+
+    //Validar la extension
+    const extensionesValidas = [
+        'png', 'jpg', 'jpeg', 'gif'
+    ];
+
+    if(!extensionesValidas.includes(extension)){
+        return res.status(400).json({
+            msg:`La extensión ${extension} no es válida. Extensiones válidas: ${extensionesValidas}`
+        });
+    }
+    res.json({
+        extension
+    })
+    /*const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
     console.log(__dirname);
     // Use the mv() method to place the file somewhere on your server
     archivo.mv(uploadPath, function (err) {
@@ -20,7 +37,7 @@ const cargarArchivo = (req = request, res = response) => {
         res.json({
             msg:'Fichero movido' + uploadPath
         })
-    });
+    });*/
 }
 
 module.exports = {
