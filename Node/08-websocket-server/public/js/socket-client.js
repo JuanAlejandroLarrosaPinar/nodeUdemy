cbb781c6477473ad88c476ac133f7875c5648afe
jalarrosap <jalarrosap@eservicios.indra.es>
2022-09-03 11:56:39 +0200

    205. Mensajes de conexión y desconexión - Cliente

--- Node/08-websocket-server/public/js/socket-client.js
+++ Node/08-websocket-server/public/js/socket-client.js
@@ -1,2 +1,16 @@
 const socket = io();
 
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+socket.on('connect', ()=>{
+    console.log('Conectado');
+    lblOnline.style.display='';
+    lblOffline.style.display='none';
+});
+
+socket.on('disconnect',()=>{
+    console.log('Desconectado');
+    lblOnline.style.display='none';
+    lblOffline.style.display=''; 
+})
