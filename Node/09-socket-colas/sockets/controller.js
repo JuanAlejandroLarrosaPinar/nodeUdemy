2f23317a83a40c9ca5518f1f7bedba6b341a3ac7
jalarrosap <jalarrosap@eservicios.indra.es>
2022-09-04 10:57:10 +0200

    220. Mostrar cola de tickets en pantalla

--- Node/09-socket-colas/sockets/controller.js
+++ Node/09-socket-colas/sockets/controller.js
@@ -5,7 +5,7 @@ const ticketControl = new TicketControl();
 const socketController = (socket) => {
 
     socket.emit('ultimo-ticket', ticketControl.ultimo);
-
+    socket.emit('estado-actual', ticketControl.ultimos4);
     socket.on('disconnect', (socket) => {
         //console.log(`Cliente desconectado ${socket.id}`);
     });
@@ -25,7 +25,7 @@ const socketController = (socket) => {
         }
 
         const ticket = ticketControl.atenderTicket(escritorio);
-        
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
         if(!ticket){
             return callback({
                 ok: false,
